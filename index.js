4278a59208cb579754d9a62d1623ef1363e9391e
PocketCoder <jake.williams77@gmail.com>
2022-04-08 11:34:36 +0100

    Express serve

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
