4345d275e8d059f80d4edaed3b1489ecba150fc8
PocketCoder <jake.williams77@gmail.com>
2022-05-12 19:31:18 +0100

    Less jQuery, new IDs

--- public/assets/dom.js
+++ public/assets/dom.js
@@ -1,5 +1,4 @@
 'use strict';
-var _a;
 let panInst;
 const mapEl = document.getElementById('map');
 function storageAvailable(type) {
@@ -13,22 +12,17 @@ function storageAvailable(type) {
 	} catch (e) {
 		return (
 			e instanceof DOMException &&
-			// everything except Firefox
 			(e.code === 22 ||
-				// Firefox
 				e.code === 1014 ||
-				// test name field too, because code might not be present
-				// everything except Firefox
 				e.name === 'QuotaExceededError' ||
-				// Firefox
 				e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-			// acknowledge QuotaExceededError only if there's something already stored
 			storage &&
 			storage.length !== 0
 		);
 	}
 }
 function loadMapData() {
+	var _a, _b;
 	try {
 		if (!storageAvailable('localStorage')) {
 			throw 'No localStorage available';
@@ -37,12 +31,14 @@ function loadMapData() {
 		alert("Sorry, local storage isn't available in your browser. That means we can't save the data you upload.");
 	} finally {
 		if (localStorage.getItem('stations') !== null) {
-			$('#js-welcome').css('display', 'none');
+			document.getElementById('js-welcome').style.display = 'none';
 			addStnsToMap(usrData('get', 'stations'));
 			updateLineSegs();
 		} else {
-			$('#js-footer').toggleClass('aside-active');
-			$('aside').toggleClass('aside-out');
+			(_a = document.getElementById('js-footer')) === null || _a === void 0
+				? void 0
+				: _a.classList.toggle('aside-active');
+			(_b = document.getElementById('js-aside')) === null || _b === void 0 ? void 0 : _b.classList.toggle('aside-out');
 		}
 	}
 }
@@ -55,11 +51,11 @@ function loadMap() {
 			setTimeout(() => {
 				elem.style.opacity = '1';
 			}, 500);
-			// @ts-ignore
 			panInst = panzoom(elem, {
 				filterKey: () => {
 					return true;
 				},
+				autoCenter: true,
 				maxZoom: 4,
 				minZoom: 1,
 				initialX: 300,
@@ -78,57 +74,25 @@ document.onreadystatechange = (e) => {
 		loadMap();
 	}
 };
-window.onload = () => {
-	var _a;
-	let isMobile = false;
-	if (
-		/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
-			navigator.userAgent
-		) ||
-		/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-			navigator.userAgent.substring(0, 4)
-		)
-	) {
-		isMobile = true;
-	}
-	if (isMobile) {
-		(_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add('mobile');
-	}
-};
-/*
-function centreOn(stn) {
-  let x, y;
-  const wC = $(window).height() / 2;
-  const hC = $(window).width() / 2;
-  try {
-    x = document.querySelector(`[id*="IC_${stations[stn]}"]`).getBoundingClientRect().x;
-    y = document.querySelector(`[id*="IC_${stations[stn]}"]`).getBoundingClientRect().y;
-  } catch (e) {
-    console.error(e);
-  }
-  const diffX = wC - x;
-  const diffY = hC - y;
-  console.log(`x: ${x}, y: ${y}; hC: ${hC}, wC: ${wC}; diffX: ${diffX}, diffY: ${diffY}`);
-  panInst.pan(diffX, diffY, {
-    relative: true
-  });
-}*/
 window.onkeyup = (e) => {
+	var _a;
 	if (e.key === '/' || e.keyCode === 191) {
-		if (!$('aside').hasClass('aside-out')) {
-			$('aside').addClass('aside-out');
-		}
-		$('.js-stn-input').focus();
+		(_a = document.getElementById('js-stnInput')) === null || _a === void 0 ? void 0 : _a.focus();
 	}
 };
 $('#js-menu').on('click', () => {
-	$('#js-footer').toggleClass('aside-active');
-	$('aside').toggleClass('aside-out');
+	var _a, _b;
+	(_a = document.getElementById('js-footer')) === null || _a === void 0 ? void 0 : _a.classList.toggle('aside-active');
+	(_b = document.getElementById('js-aside')) === null || _b === void 0 ? void 0 : _b.classList.toggle('aside-out');
 });
 $('.js-stn-input').on('keyup', (e) => {
+	var _a;
 	if (e.key === 'Enter' || e.keyCode === 13) {
-		if (newStation($('.js-stn-input').val().toString())) {
-			$('.js-stn-input').addClass('confirm-animate');
+		const stn = document.getElementById('js-stnInput').value;
+		if (newStation(stn)) {
+			(_a = document.getElementById('js-stnInput')) === null || _a === void 0
+				? void 0
+				: _a.classList.add('confirm-animate');
 		}
 	}
 });
@@ -138,9 +102,8 @@ function newStation(input) {
 	updateLineSegs();
 	return true;
 }
-// @ts-ignore
 const autoCompleteJS = new autoComplete({
-	selector: '.js-stn-input',
+	selector: '#js-stnInput',
 	wrapper: true,
 	threshold: 2,
 	data: {
@@ -634,14 +597,3 @@ const autoCompleteJS = new autoComplete({
 		},
 	},
 });
-(_a = document.querySelector('.js-stn-input')) === null || _a === void 0
-	? void 0
-	: _a.addEventListener('selection', function (event) {
-			// @ts-ignore
-			if (newStation(event.detail.selection.value)) {
-				$('.js-stn-input').addClass('confirm-animate');
-				setTimeout(() => {
-					$('.js-stn-input').removeClass('confirm-animate').val('');
-				}, 2250);
-			}
-	  });
